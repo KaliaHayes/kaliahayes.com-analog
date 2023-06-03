54b6ba1f4dacb3359f1c8929638af720f4b40154
Kalia Hayes <kaliahayes@yahoo.com>
2023-06-02 20:03:17 -0500

    footer added

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -6,8 +6,20 @@ import { CommonModule, NgForOf } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <div class="small shimmer footer">
-      <p>goodbye world</p>
+    <div class="small footer">
+      <p class="shimmer">Kalia Hayes © 2023</p>
+      <p>Powered by Analog & Vercel</p>
+      <div class="social-icons flex">
+        <a href="https://github.com/KaliaHayes" target="_blank">
+          <img src="github-svgrepo-com.svg" alt="" />
+        </a>
+        <a href="https://www.linkedin.com/in/kaliahayes/" target="_blank">
+          <img src="linkedin-svgrepo-com.svg" alt="" />
+        </a>
+        <a href="https://twitter.com/KaliaHayes" target="_blank">
+          <img src="twitter-svgrepo-com.svg" alt="" />
+        </a>
+      </div>
     </div>
   `,
   styles: [
@@ -16,6 +28,27 @@ import { CommonModule, NgForOf } from '@angular/common';
         text-align: center;
         max-width: 600px;
         margin: 50px auto;
+      }
+
+      .footer p:nth-child(2) {
+        color: #b9b9b9;
+      }
+
+      .social-icons {
+        justify-content: center;
+        margin-top: 10px;
+      }
+
+      .social-icons img {
+        filter: invert(0.2);
+        width: 25px;
+        margin-left: 10px;
+      }
+
+      .social-icons img:hover {
+        cursor: pointer;
+        filter: invert(1);
+        transition: all 0.2s ease-in-out;
       }
     `,
   ],
